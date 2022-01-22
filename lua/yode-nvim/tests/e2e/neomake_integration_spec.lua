d9600b3702c9216ec19346a7585583166d5fe89d
Stefan Gojan <vollekannehoschi@gmail.com>
2022-01-22 12:36:52 +0100

    add spec for Neomake integration

---
+++ lua/yode-nvim/tests/e2e/neomake_integration_spec.lua
@@ -0,0 +1,13 @@
+describe('Neomake integration -', function()
+    pending('entering buffer with error should have Neomake signs')
+    pending(
+        'solving error in file buffer should remove Neomake sign from it and all visible seditors containing that line'
+    )
+    pending(
+        'solving error in seditor should remove Neomake sign from it and all visible seditors containing that line and visible file buffer'
+    )
+    pending(
+        'solving error in seditor with file editor in another tab should remove signs when visiting it afterwards'
+    )
+    pending('same for adding error in file buffer again and going back to seditor tab')
+end)
